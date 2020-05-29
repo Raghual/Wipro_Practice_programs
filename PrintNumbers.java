5b42fefc4e77ea86c296b0f71ebdc8eba74d1a9f
Raghual R <raviraghual@gmail.com>
2020-05-29 08:35:24 +0530

    Add files via upload

---
+++ PrintNumbers.java
@@ -0,0 +1,14 @@
+package myproject;
+
+public class PrintNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("1");
+		for(int i=2;i<=10;i++) {
+			System.out.print("\t"+i);
+		}
+
+	}
+
+}
