1291a3f92a546b22e44f543fac2d3f84a56987a2
Raghual R <raviraghual@gmail.com>
2020-06-15 07:53:43 +0530

    Create Inheritance2.1.java

---
+++ Inheritance2.1.java
@@ -0,0 +1,16 @@
+package myproject;
+
+ class Person {
+	String nam;
+	Person(String name){
+		nam=name;
+	}
+	public String getNam() {
+		return nam;
+	}
+	public void setNam(String nam) {
+		this.nam = nam;
+	}
+	
+
+}
